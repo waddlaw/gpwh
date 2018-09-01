d977c2426611b0e6616ca3d3bd760fd0a8b4b1bb
waddlaw <ingronze@gmail.com>
2018-09-01 14:01:19 +0900

    15章完了

--- src/Lesson15.hs
+++ src/Lesson15.hs
@@ -10,6 +10,17 @@ module Lesson15
   , threeLetterMessage
   , threeLetterEncoder
   , threeLetterDecoder
+  -- ** 15.3
+  , intToBits'
+  , intToBits
+  , bitsToInt
+  , charToBits
+  , bitsToChar
+  , encoderDecoder
+  , encode
+  , decode
+  , myOTP
+  , examplePRNG
   ) where
 
 rotEncoder :: String -> String
@@ -90,3 +101,92 @@ threeLetterDecoder vals = map rot3ldecoder vals
   where
     alphaSize = 1 + fromEnum (maxBound :: ThreeLetterAlphabet)
     rot3ldecoder = rotNdecoder alphaSize
+
+xorBool :: Bool -> Bool -> Bool
+xorBool value1 value2 = (value1 || value2) && (not (value1 && value2))
+
+xorPair :: (Bool, Bool) -> Bool
+xorPair (v1, v2) = xorBool v1 v2
+
+xor :: [Bool] -> [Bool] -> [Bool]
+xor list1 list2 = map xorPair (zip list1 list2)
+
+type Bits = [Bool]
+
+intToBits' :: Int -> Bits
+intToBits' 0 = [False]
+intToBits' 1 = [True]
+intToBits' n = if (remainder == 0)
+               then False : intToBits' nextVal
+               else True : intToBits' nextVal
+  where
+    remainder = n `mod` 2
+    nextVal = n `div` 2
+
+maxBits :: Int
+maxBits = length (intToBits' maxBound)
+
+intToBits :: Int -> Bits
+intToBits n = leadingFalses ++ reversedBits
+  where
+    reversedBits = reverse (intToBits' n)
+    missingBits = maxBits - (length reversedBits)
+    leadingFalses = take missingBits (cycle [False])
+
+charToBits :: Char -> Bits
+charToBits char = intToBits (fromEnum char)
+
+bitsToInt :: Bits -> Int
+bitsToInt bits = sum (map (\x -> 2^(snd x)) trueLocations)
+  where
+    size = length bits
+    indices = [size-1, size-2 .. 0]
+    trueLocations = filter (\x -> fst x == True) (zip bits indices)
+
+bitsToChar :: Bits -> Char
+bitsToChar bits = toEnum (bitsToInt bits)
+
+myPad :: String
+myPad = "Shhhhhh"
+
+myPlainText :: String
+myPlainText = "Haskell"
+
+applyOTP' :: String -> String -> [Bits]
+applyOTP' pad plaintext = map (\pair -> (fst pair) `xor` (snd pair)) (zip padBits plaintextBits)
+  where
+    padBits = map charToBits pad
+    plaintextBits = map charToBits plaintext
+
+applyOTP :: String -> String -> String
+applyOTP pad plaintext = map bitsToChar bitList
+  where
+    bitList = applyOTP' pad plaintext
+
+encoderDecoder :: String -> String
+encoderDecoder = applyOTP myPad
+
+class Cipher a where
+  encode :: a -> String -> String
+  decode :: a -> String -> String
+
+data Rot = Rot
+
+instance Cipher Rot where
+  encode Rot text = rotEncoder text
+  decode Rot text = rotDecoder text
+
+data OneTimePad = OTP String
+
+instance Cipher OneTimePad where
+  encode (OTP pad) text = applyOTP pad text
+  decode (OTP pad) text = applyOTP pad text
+
+myOTP :: OneTimePad
+myOTP = OTP (cycle [minBound .. maxBound])
+
+prng :: Int -> Int -> Int -> Int -> Int
+prng a b maxNumber seed = (a*seed + b) `mod` maxNumber
+
+examplePRNG :: Int -> Int
+examplePRNG = prng 1337 7 100
